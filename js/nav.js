67e3cf8fe807c8b14b618abd085529a4be4f85f8
Armanahmed12 <mdarmanahmed1232@gmail.com>
2024-10-25 11:56:42 +0600

    Initially completed

--- js/nav.js
+++ js/nav.js
@@ -1,31 +1,41 @@
 const ulTagOfNav = document.getElementById("nav-lists-container");
-const menuBar = document.getElementById("menuBarr");
+const menuBar = document.getElementById("menuBar");
 
 // navigation bar
-document.getElementById("menuBar").addEventListener("click", () => {
+menuBar.addEventListener("click", () => {
   const styles = [...ulTagOfNav.classList];
 
   styles.forEach((style) => {
     if (style.startsWith("top-")) {
-      if (style !== "top-[53.6px]") {
+
+      if (style !== "top-[69.6px]") {
+
         ulTagOfNav.classList.remove("top-[-600px]");
-        ulTagOfNav.classList.add("top-[53.6px]");
+        ulTagOfNav.classList.add("top-[69.6px]");
         document.getElementById('bannerSection').style.zIndex = '-20';
+        // change the barIcon into X
+        menuBar.setAttribute('class', 'fa-solid fa-x text-[#1dd100] text-2xl block sm:hidden')
+       
+
       } else {
-        ulTagOfNav.classList.remove("top-[53.6px]");
+
+        ulTagOfNav.classList.remove("top-[69.6px]");
         ulTagOfNav.classList.add("top-[-600px]");
-        
-        setTimeout(() => {
+        // change the "x" icon into Menubar
+        menuBar.setAttribute('class', 'text-2xl fa-solid fa-bars block sm:hidden')
+        setTimeout( ()=>{
           document.getElementById('bannerSection').style.zIndex = '20';
-        }, 1000);
+        },1000);
+    
       }
     }
   });
 });
 
 // scroll down to the booking ticket section by clicking the btn of banner-section
-document.getElementById("sss").addEventListener("click", ()=>{
 
+
+document.getElementById("sss").addEventListener("click", ()=>{
 
   const lowerSection = document.getElementById('transportationSection');
   lowerSection.scrollIntoView({ behavior: 'smooth' });
